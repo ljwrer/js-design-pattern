ac365560f097c3207f246fe8a0feefc26fbaa244
ljwrer <ljwrer@sina.com>
2017-07-14 16:39:44 +0800

    add karma

--- test/browser/animate.spec.js
+++ test/browser/animate.spec.js
@@ -1,17 +1,17 @@
 /**
  * Created by Ray on 2017/7/11.
  */
-const Animate = require('../singleton/Animate')
+const Animate = require('../../singleton/Animate')
 const sinon = require("sinon");
 const assert = require('chai').assert;
 describe('Animate', function () {
     describe('#start',function () {
-        
+        it('should start',function () {
+            assert.isObject({})
+        })
     })
     describe('#step',function () {
-        
     })
     describe('#update',function () {
-        
     })
 })
